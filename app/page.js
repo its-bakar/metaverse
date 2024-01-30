39f9e0be3c087fab38f65b75f114d70e418b2d66
unknown <ironsamurai786@gmail.com>
2024-01-30 14:23:20 +0500

    Hero section

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar";
+import Hero from "@/sections/Hero";
 
 export default function Home() {
   return (
     <div className="bg-primary-black overflow-hidden">
       <Navbar />
+      <Hero/>
     </div>
   );
 }
