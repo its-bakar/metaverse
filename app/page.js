8eca9cd2dc0665e96ca2809c87723e79dddeadd8
unknown <ironsamurai786@gmail.com>
2024-01-31 02:28:43 +0500

    insights section

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import About from "@/sections/About";
 import Explore from "@/sections/Explore";
 import GetStarted from "@/sections/GetStarted";
 import Hero from "@/sections/Hero";
+import Insights from "@/sections/Insights";
 import WhatsNew from "@/sections/WhatsNew";
 import World from "@/sections/World";
 
@@ -22,6 +23,7 @@ export default function Home() {
       <WhatsNew/>
       </div>
       <World/>
+      <Insights/>
     </div>
   );
 }
